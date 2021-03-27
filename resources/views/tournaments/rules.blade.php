2afc64d773b32319d10664ba69b788ed4b5fd770
Mathieu <degracia.mathieu@gmail.com>
2021-03-27 23:27:08 +0100

    hydrate rules

---
+++ resources/views/tournaments/rules.blade.php
@@ -0,0 +1,17 @@
+<p>:one: Installer Fightcade</p>
+<p>- Télécharger et installer Fightcade2 : https://www.fightcade.com/</p>
+<br>
+<p>:two: Se créer un compte</p>
+<p>- Pour pouvoir vous connecter à FightCade2 vous devez vous créer un compte sur le site https://www.fightcade.com/register</p>
+<br>
+<p>:three: Installer Windjammers</p>
+<p>- Lancer Fightcade 2, puis cliquer sur ROMS > Open ROMS Folder</p>
+<p>- Dans la nouvelle fenêtre cliquez sur  le  lien FBNeo ROMs, ce lien vous emmène vers le dossier contenant les ROMs de l'émulateur (FightCade\emulator\fbneo\ROMs)</p>
+<p>- Dans ce dossier insérer les archives neogeo.zip et wjammers.zip</p>
+<p>- Relancer FightCade2 pour prendre en compte les nouvelles ROMs</p>
+<br>
+<p>:four: Ajouter le looby du tournoi</p>
+<p>- Une fois connecté à Fighcade2 ajouter le loobie du tournoi en recherchant l'identifiant : Windjammers FC2 (voir vidéo)</p>
+<br>
+<p>:five: Préparer son compte aux matchs ranked</p>
+<p>- Pour pouvoir participer aux matchs il est nécessaire de paramétrer son compte en "Ranked Player" (voir vidéo)</p>
