8841137cdf8ef81d7bf81f9e88624691647e385d
Mathieu <degracia.mathieu@gmail.com>
2021-03-29 12:27:03 +0200

    static version

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -10,11 +10,31 @@ class TournamentController extends Controller
 {
     public function index(Request $request)
     {
-        $tournament = Tournament::select('slug')->latest()->first();  
+        $tournament['title'] = 'Winter Cup 2021';
+        $tournament['challonge_url'] = 'https://windjammersfr.challonge.com/fr/winterleague3';
+        $tournament['started_at'] = '2021-01-10 21:00 CET';
+        $tournament['localization']['description'] = 'blablabla c la description en francais Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+        $tournament['localization']['rules'] = [
+            'qsd qsd qsd sqd qsd q',
+            'qsd qsd qsd sqd qsd q',
+            'qsd qsd qsd sqd qsd q',
+        ];
+        $tournament['video'] = 'https://www.youtube.com/watch?v=pMT9SYqoX94';
+        $tournament['winners'] = [
+            'qsdsq',
+            'qsdsq',
+            'qsdsq',
+        ];
+             
+        return View::make('tournaments.show', [
+            'tournament' => $tournament,
+        ]); 
+        
+        // $tournament = Tournament::select('slug')->latest()->first();  
 
-        return redirect()->route('show', [
-            'tournament_slug' => $tournament->slug,
-        ]);
+        // return redirect()->route('show', [
+        //     'tournament_slug' => $tournament->slug,
+        // ]);
     }
 
     public function show(Tournament $tournament)
