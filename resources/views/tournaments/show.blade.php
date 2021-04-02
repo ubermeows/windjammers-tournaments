77e1e8005a2c5f6486e6e81cef9ff7347ca11d0c
marionj <marionj@focus-home.com>
2021-04-02 16:15:35 +0200

    centered button

--- resources/views/tournaments/show.blade.php
+++ resources/views/tournaments/show.blade.php
@@ -23,7 +23,7 @@
             </ul>
             <p class="mt-3">Si tu ne sais pas encore comment nous rejoindre le jour du tournoi, jette un coup d'œil à <a href="{{route('rules')}}" class="underline text-indigo-500">la procédure</a>.</p>
         </div>
-        <div class="mt-6">
+        <div class="mt-8 text-center">
             <a href="{{$tournament['challonge_url']}}" rel="noreferrer nofollow noopener" target="_blank" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-blue-700 bg-blue-100 border border-transparent rounded-md shadow-sm hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                 Participer au tournoi
             </a>
