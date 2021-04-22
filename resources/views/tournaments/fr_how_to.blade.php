6ce15db9558e6bb985ffa3901691ca4b97c63cc1
marionj <marionj@focus-home.com>
2021-04-22 18:54:10 +0200

    francé round 2

--- resources/views/tournaments/fr_how_to.blade.php
+++ resources/views/tournaments/fr_how_to.blade.php
@@ -13,9 +13,9 @@
     <div class="max-w-3xl mx-auto prose">
         <h1>How to 🥏</h1>
         <h2>1. Installer Fightcade</h2>
-        <p>Télécharger et installer <b>Fightcade2</b> : <a href="https://www.fightcade.com/">https://www.fightcade.com/</a></p>
+        <p>Téléchargez et installez <b>Fightcade2</b> : <a href="https://www.fightcade.com/">https://www.fightcade.com/</a>.</p>
         <h2>2. Se créer un compte</h2>
-        <p>Pour pouvoir vous connecter à <b>Fightcade2</b> vous devez vous créer un compte sur le site <a href="https://www.fightcade.com/register">https://www.fightcade.com/register</a></p>
+        <p>Pour pouvoir vous connecter à <b>Fightcade2</b>, vous devez vous créer un compte sur le site <a href="https://www.fightcade.com/register">https://www.fightcade.com/register</a>.</p>
         <h2>3. Installer Windjammers</h2>
         <ul>
             <li>Lancez <b>Fightcade 2</b>, puis cliquez sur <code>ROMS > Open ROMS Folder</code></li>
@@ -24,13 +24,13 @@
             <li>Relancez <b>Fightcade2</b> pour prendre en compte les nouvelles ROMs</li>
         </ul>
         <h2>4. Ajouter le looby du tournoi</h2>
-        <p>Une fois connecté à <b>Fighcade2</b> ajoutez le loobie du tournoi en recherchant l'identifiant : <b>Windjammers</b></p>
-        <p><b>Attention</b> à bien selectionner le lobbie <b>ARCADE FC2</b></p>
+        <p>Une fois connecté à <b>Fighcade2</b>, ajoutez le loobie du tournoi en recherchant l'identifiant : <b>Windjammers</b>.</p>
+        <p><b>Attention</b> à bien selectionner le lobby <b>ARCADE FC2</b>.</p>
         <video controls width="500">
             <source src="{{asset('storage/webm/loobie.webm')}}" type="video/webm">
         </video>
         <h2>5. Préparer son compte aux matchs ranked</h2>
-        <p>Pour participer aux matchs il est préférable de paramétrer son compte en "Ranked Player"</p>
+        <p>Pour participer aux matchs il est préférable de paramétrer son compte en "Ranked Player".</p>
         <video controls width="500">
             <source src="{{asset('storage/webm/rank.webm')}}" type="video/webm">
         </video>
