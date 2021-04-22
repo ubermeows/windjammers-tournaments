46c51b6069620c5aacdb5005e992c5a9a5ea25a9
marionj <marionj@focus-home.com>
2021-04-22 18:50:05 +0200

    c pa facil le francer

--- resources/views/tournaments/fr_how_to.blade.php
+++ resources/views/tournaments/fr_how_to.blade.php
@@ -18,13 +18,13 @@
         <p>Pour pouvoir vous connecter à <b>Fightcade2</b> vous devez vous créer un compte sur le site <a href="https://www.fightcade.com/register">https://www.fightcade.com/register</a></p>
         <h2>3. Installer Windjammers</h2>
         <ul>
-            <li>Lancez <b>Fightcade 2</b>, puis cliquer sur <code>ROMS > Open ROMS Folder</code></li>
+            <li>Lancez <b>Fightcade 2</b>, puis cliquez sur <code>ROMS > Open ROMS Folder</code></li>
             <li>Dans la nouvelle fenêtre cliquez sur le lien <code>FBNeo ROMs</code>, ce lien vous emmène vers le dossier contenant les ROMs de l'émulateur</li>
-            <li>Dans ce dossier insérer les archives <b><a href="{{asset('storage/roms/neogeo.zip')}}">neogeo.zip</a></b> et <b><a href="{{asset('storage/roms/wjammers.zip')}}">wjammers.zip</a></b></li>
-            <li>Relancer <b>Fightcade2</b> pour prendre en compte les nouvelles ROMs</li>
+            <li>Dans ce dossier, insérez les archives <b><a href="{{asset('storage/roms/neogeo.zip')}}">neogeo.zip</a></b> et <b><a href="{{asset('storage/roms/wjammers.zip')}}">wjammers.zip</a></b></li>
+            <li>Relancez <b>Fightcade2</b> pour prendre en compte les nouvelles ROMs</li>
         </ul>
         <h2>4. Ajouter le looby du tournoi</h2>
-        <p>Une fois connecté à <b>Fighcade2</b> ajouter le loobie du tournoi en recherchant l'identifiant : <b>Windjammers</b></p>
+        <p>Une fois connecté à <b>Fighcade2</b> ajoutez le loobie du tournoi en recherchant l'identifiant : <b>Windjammers</b></p>
         <p><b>Attention</b> à bien selectionner le lobbie <b>ARCADE FC2</b></p>
         <video controls width="500">
             <source src="{{asset('storage/webm/loobie.webm')}}" type="video/webm">
